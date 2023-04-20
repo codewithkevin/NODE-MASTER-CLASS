e09b300b10b1387c48a0aeaed64655b80d58424d
codewithkevin <oseikelvin709@gmail.com>
2023-04-20 09:24:06 +0000

    Sitting Up application dependencies

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+var port = process.env.PORT || 3000;
+
+//Intialize Express Express
+var app = express();
+
+app.listen(port, function () {
+  console.log("Server is running at http://localhost:" + port);
+});
