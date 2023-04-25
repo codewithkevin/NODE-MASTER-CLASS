01d4891a35bdb73d7143d34eae3e368d8f2feba6
codewithkevin <oseikelvin709@gmail.com>
2023-04-25 15:23:46 +0000

    Update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import subjects from "./routes/courses.js";
+import helmet from "helmet";
+import morgan from "morgan";
 
 dotenv.config();
 
@@ -8,11 +10,19 @@ const port = process.env.PORT || 3000;
 
 // Initialize Express
 const app = express();
+const state = app.get("env");
+
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(helmet());
+
+if (state === "development") {
+  app.use(morgan("tiny"));
+  console.log("Morgan enabled......");
+}
 
 app.use("/api/courses", subjects);
 
