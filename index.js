51d576aebe87529a6dd233bf18df6777aa565261
codewithkevin <oseikelvin709@gmail.com>
2023-04-25 05:07:48 +0000

    Working with env

--- index.js
+++ index.js
@@ -3,17 +3,25 @@ import dotenv from "dotenv";
 import joi from "joi";
 import { logger } from "./Middleware/logger.js";
 import helmet from "helmet";
+import morgan from "morgan";
 
 dotenv.config();
+const app = express();
 
 const port = process.env.PORT || 3000;
 
-// Initialize Express
-const app = express();
+const state = app.get("env");
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(helmet());
+
+if (state === "development") {
+  app.use(morgan("tiny"));
+  console.log("Morgan enabled......");
+}
 
 app.use(logger);
 
